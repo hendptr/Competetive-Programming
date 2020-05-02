83a7aba8142934b962169ae126999ae83257a83e
Hendriawan Putra <49605969+hendptr@users.noreply.github.com>
2020-05-02 13:56:18 +0800

    Create Hackerrank - Grading Students.cpp

---
+++ Hackerrank - Grading Students.cpp
@@ -0,0 +1,16 @@
+#include <cmath>
+#include <iostream>
+
+int main() {
+    int n, tmp_grade;
+    
+    std::cin >> n;
+    while(n-->0) {
+        std::cin >> tmp_grade;
+        if (tmp_grade >= 38 && tmp_grade % 5 > 2)
+            tmp_grade += 5 - (tmp_grade % 5);
+        std::cout << tmp_grade << '\n';
+    }
+    
+    return 0;
+}
